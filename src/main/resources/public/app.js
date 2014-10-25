79e94f9b98314cea3b59ede785c64850930fbd17
Ian Macalinao <ian@giza.us>
2014-10-25 08:06:40 -0500

    Add update lists method

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -1,31 +1,63 @@
 angular.module('projections', ['ui.router'])
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-  .state('home', {
-    url: '/',
-    templateUrl: 'templates/home.html',
-    controller: 'HomeCtrl'
-  })
-  .state('stock', {
-    url: '/stock/:stock',
-    templateUrl: 'templates/stock.html',
-    controller: 'StockCtrl'
-  });
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'templates/home.html',
+        controller: 'HomeCtrl'
+      })
+      .state('stock', {
+        url: '/stock/:stock',
+        templateUrl: 'templates/stock.html',
+        controller: 'StockCtrl'
+      });
 
-  $urlRouterProvider.otherwise('/');
-})
-.factory('fields', function() {
-  return {
-    last: 'Last',
-    high: 'High',
-    low: 'Low',
-    close: 'Close',
-    newsSentiment: 'News Sentiment',
-    twitterSentiment: 'Twitter Sentiment',
-    eqyRecCons: 'Buy Rating'
-  };
-})
-.controller('HomeCtrl', function($scope, fields) {
-  $scope.fields = fields;
-  $scope.field = fields.eqyRecCons;
-});
+    $urlRouterProvider.otherwise('/');
+  })
+  .factory('fields', function() {
+    return [{
+      id: 'eqyRecCons',
+      name: 'Buy Rating'
+    }, {
+      id: 'last',
+      name: 'Last'
+    }, {
+      id: 'high',
+      name: 'High'
+    }, {
+      id: 'low',
+      name: 'Low'
+    }, {
+      id: 'close',
+      name: 'Close'
+    }, {
+      id: 'newsSentiment',
+      name: 'News Sentiment'
+    }, {
+      id: 'twitterSentiment',
+      name: 'Twitter Sentiment'
+    }];
+  })
+
+  .controller('HomeCtrl', function($scope, fields) {
+    $scope.fields = fields;
+    $scope.field = fields[0];
+
+    $scope.best = [];
+    $scope.worst = [];
+
+    function updateLists() {
+      var fieldId = $scope.field.id;
+      $http.get('/api/all').success(function(data) {
+        $scope.best = _.first(data.sort(function(a, b) {
+          return b[fieldId] - a[fieldId];
+        }), 10);
+        $scope.worst = _.first(data.sort(function(a, b) {
+          return a[fieldId] - b[fieldId];
+        }), 10);
+      });
+    }
+
+    $scope.$watch('field', updateLists);
+    updateLists();
+  });
